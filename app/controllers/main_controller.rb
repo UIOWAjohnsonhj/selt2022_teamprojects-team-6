a0fb81aafc88fdf28723b13627a0b4befca8afe3
aezouhri <adnane-ezouhri@uiowa.edu>
2022-11-07 13:07:02 -0600

    Able to check which values are missing from the form. Will need some help with the redirect and flash messages.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -17,4 +17,32 @@ class MainController < ApplicationController
   def view_profile
 
   end
+
+
+  def intermediate_login
+    puts params[:user_first_name]
+    if params[:user_first_name]==nil
+      puts "first"
+      flash[:warning]= "Empty first name"
+    end
+    if params[:user_last_name]==nil
+      puts "last"
+      flash[:warning]= "Empty last name"
+    end
+    if params[:user_email]==nil
+      puts "email"
+      flash[:warning]= "Empty first name"
+    end
+    if params[:user_password]==nil
+      puts "pwd"
+      flash[:warning]= "Empty first name"
+    end
+    if params[:type].blank?
+      puts "radio"
+      flash[:warning]= "Empty radio"
+    end
+
+    # redirect_to main_sign_up_path
+  end
+
 end
