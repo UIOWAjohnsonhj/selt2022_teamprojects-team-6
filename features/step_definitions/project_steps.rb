77239d795be5fb010dcb8d7649b0a6ac9b99e5bd
kianaerickson <kiana-erickson@uiowa.edu>
2022-11-09 12:03:30 -0600

    all create_account cucumber tests but last redirect cucumber have been written

--- features/step_definitions/project_steps.rb
+++ features/step_definitions/project_steps.rb
@@ -1,6 +1,40 @@
 Given(/^I am on the Account Creation page$/) do
-  pending
+  visit main_sign_up_path
 end
+
+And(/^I fill in "([^"]*)" with "([^"]*)"$/) do |arg1, arg2|
+  fill_in arg1, :with => arg2
+end
+
+And(/the student radio button is clicked/) do
+  click_button 'radio_button_student'
+end
+
+Then(/^A new student account should be created with first name "(.*?)", last name "(.*?)", and email address "(.*?)"$/) do |first, last, email|
+  result=false
+  all("tr").each do |tr|
+    if tr.has_content?(first) && tr.has_content?(last) && tr.has_content?(email)
+      result = true
+      break
+    end
+  end
+  expect(result).to be_truthy
+end
+
+And(/the faculty radio button is clicked/) do
+  click_button 'radio_button_faculty'
+end
+
+Then (/^I should be directed to the second sign up page to enter faculty specific information$/) do
+
+end
+
+
+
+
+
+
+
 
 And(/^my credentials are filled in$/) do
   pending
@@ -10,9 +44,7 @@ When(/^I Click "([^"]*)"$/) do |arg|
   pending
 end
 
-Then(/^A new account should be created$/) do
-  pending
-end
+
 
 Given(/^I am an Instructor$/) do
   pending
@@ -78,6 +110,4 @@ And(/^my credentials are filled in with an email address already registered$/) d
   pending
 end
 
-And(/^I fill in "([^"]*)" with "([^"]*)"$/) do |arg1, arg2|
-  pending
-end
+
