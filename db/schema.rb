f8d89984d0d2e1ed599ba3edf00dcb18e143ce0d
Adnane Ezouhri <adnane-ezouhri@uiowa.edu>
2022-11-09 20:02:51 -0600

    Before PR

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20221110000653) do
+ActiveRecord::Schema.define(version: 20221108040054) do
 
   create_table "experiences", force: :cascade do |t|
     t.integer "student_id"
@@ -27,23 +27,19 @@ ActiveRecord::Schema.define(version: 20221110000653) do
     t.string  "last_name"
     t.string  "user_name"
     t.string  "email"
-    t.string  "password"
+    t.string  "password_digest"
     t.string  "department"
     t.boolean "chair"
     t.string  "university"
-    t.string  "password"
-    t.string  "string"
   end
 
   create_table "profiles", force: :cascade do |t|
     t.integer "student_id"
     t.integer "gre"
     t.integer "toefl"
-    t.integer "capa"
     t.string  "interested_major"
     t.string  "term"
     t.string  "year"
-    t.string  "college_name"
   end
 
   create_table "students", force: :cascade do |t|
@@ -51,11 +47,9 @@ ActiveRecord::Schema.define(version: 20221110000653) do
     t.string   "last_name"
     t.string   "user_name"
     t.string   "email"
-    t.string   "password_digest"
+    t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "password"
-    t.string   "string"
   end
 
 end
