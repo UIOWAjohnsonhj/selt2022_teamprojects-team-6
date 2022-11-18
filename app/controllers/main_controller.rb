5b67d2aceb956d46a747b1705d5aa014b817f445
christianBaseme <cbaseme@uiowa.edu>
2022-11-17 22:02:22 -0600

    Added the password authentication to the login, the login won't allow you to with just email, test case: tested with wrong passwords, empty fields, and correct credentials; all passed. Improvements needed for using the actual authenticate method provided by has_secure_password, attempted but the routes were wrong, paused for now!

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -28,6 +28,7 @@ class MainController < ApplicationController
     @current_profile = Profile.where(student_id: @@id).take
     #@experience = Experience.where(student_id: @id).take
     puts @@id,"View"
+
     @student = Student.find(@@id)
 
     if params.include? "gre"
@@ -40,6 +41,7 @@ class MainController < ApplicationController
       @current_profile.college_name = params[:college_name]
       @current_profile.save
     end
+
   end
 
 
@@ -124,21 +126,45 @@ class MainController < ApplicationController
   end
   def intermediate_login
     given_email= params[:user][:email]
+    given_password = params[:user][:password]
 
-    @student = Student.where(email: given_email).take
-    @faculty = Faculty.where(email: given_email).take
-   if not @student.nil?
-      @@id = @student.id
-      redirect_to view_profile_path
-   elsif not @faculty.nil?
-     @@id = @faculty.id
-     redirect_to faculty_profile_path
+    #@student = Student.where( email:given_email).take
 
-   else
+    #@faculty = Faculty.where( email: given_email).take
+    #puts 'line 134: ', @student
+    #p 'line 135  ', @faculty
+    #if (not @student.nil?) || (not @faculty.nil?)
+    @student = Student.find_by(email:given_email,password_digest:given_password)
+    @faculty = Faculty.find_by(email:given_email,password_digest:given_password)
+
+    begin
+      puts 'line 139'
+      if not @student.nil? #student1 && student1.authenticate(given_password)
+        #&.authenticate(given_password)
+        puts 'line 141'
+
+        @@id = @student.id
+        #p @student
+        redirect_to view_profile_path
+      #elsif not @faculty.nil?
+      elsif not @faculty.nil?
+        #&.authenticate(given_password)
+        puts 'line 148'
+        @@id = @faculty.id
+        redirect_to faculty_profile_path
+
+      else
          flash[:notice]="Invalid user"
          redirect_to login_path
-   end
+      end
+    rescue BCrypt::Errors::InvalidHash
+      flash[:error] = 'We recently adjusted the way our passwords are store. Please Reset your password '
+    end
+    #else
+    # flash[:notice]="Invalid user 2"
+    #  redirect_to login_path
 
+    #end
   end
   def reset_password
     #redirect_to reset_password_path
