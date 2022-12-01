a1fded3b40fb812cba44f516a749fd2fe84a4433
saanbe16 <joslin-some@uiowa.edu>
2022-11-30 22:49:31 -0600

    final sprint changes

--- config/application.rb
+++ config/application.rb
@@ -5,8 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-# config/application.rb
-config.assets.initialize_on_precompile = false
+
 module Rottenpotatoes
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -23,5 +22,7 @@ module Rottenpotatoes
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    # config/application.rb
+    config.assets.initialize_on_precompile = false
   end
 end
