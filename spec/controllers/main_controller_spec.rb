061ba25b0d9c549e07873680f2ee833a78e26e2b
erbrewer <erbrewer@uiowa.edu>
2022-12-04 13:31:27 -0600

    TST: main routing tests passing for MainController pages.

--- spec/controllers/main_controller_spec.rb
+++ spec/controllers/main_controller_spec.rb
@@ -12,24 +12,34 @@ if RUBY_VERSION >= '2.6.0'
       end
     end
   else
-    puts "Monkeypatch for ActionController::TestResponse no longer needed"
   end
 end
 
 describe MainController do
   describe 'home page' do
     it 'should show the home/index page' do
-      allow
+      allow(MainController).to receive(:index)
+      get :index
+      expect(response).to render_template('/')
     end
   end
   describe 'login page' do
     it 'should show the login page' do
-
+      allow(MainController).to receive(:login)
+      get :login
+      expect(response).to render_template('login')
     end
   end
   describe 'signup page' do
-    it 'should show the signup page' do
-
+    it 'should show the signup page for students' do
+      allow(MainController).to receive(:sign_up)
+      get :sign_up
+      expect(response).to render_template('sign_up')
+    end
+    it 'should show the signup page for faculty' do
+      allow(MainController).to receive(:sign_up_faculty)
+      get :sign_up_faculty
+      expect(response).to render_template('sign_up_faculty')
     end
   end
 end
