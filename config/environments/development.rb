3d03cd7130b39d49e43bbe4dc40587a2fd99f160
christianBaseme <cbaseme@uiowa.edu>
2022-12-05 17:04:25 -0600

    Working on using email to send the password reset link, right now not working still figuring out

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,4 +42,20 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   #config.action_mailer.default_url_options = {host: 'localhost:3000'}
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: ENV["GMAIL_USERNAME"]}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'localhost:3000', protocol: 'http'}
+  config.action_mailer.smtp_settings = {
+    address:   'smtp.gmail.com',
+    port:      587,
+    domain:    "https://cryptic-waters-88526.herokuapp.com",
+    username:  ENV["malikystore@gmail.com"],
+    password:  ENV["19$Faster100"],
+    authentication: "plain",
+    enable_starttls_auto: true
+
+  }
 end
