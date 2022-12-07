2f89fbb82f1fd8db14942a39d932f145e7942fe2
aezouhri <adnane-ezouhri@uiowa.edu>
2022-12-07 11:48:45 -0600

    Generated the different mailer files

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'malikystore@outlook.com'
+  default from: 'ezouhriadnane@outlook.com'
   layout 'mailer'
 end
 
---
+++ app/mailers/password_recovery_mailer.rb
@@ -0,0 +1,13 @@
+class PasswordRecoveryMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.password_recovery_mailer.password_recovery.subject
+  #
+  def password_recovery
+    @greeting = "Hi"
+
+    mail to: "ezouhriadnane@gmail.com"
+  end
+end
