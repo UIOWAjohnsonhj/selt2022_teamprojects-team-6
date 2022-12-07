673e46220dc75eb13eec14f347899053791ea723
erbrewer <erbrewer@uiowa.edu>
2022-12-07 17:58:30 -0600

    TST: removing old tests so that they can be rewritten with devise.

--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -17,59 +17,21 @@ if RUBY_VERSION >= '2.6.0'
 end
 
 describe MainController do
-  describe 'home page' do
-    it 'should call the model that shows profile info' do
-      allow(MainController).to receive(:view_profile)
-      post :view_profile
-      expect(response).to render_template('view_profile')
-    end
+  # Home Page
+  describe 'Home Page' do
+
   end
-  it 'should make the profiles info available to that template' do
-    fake_results = [double('Profile'), double('Profile')]
-    allow(MainController).to receive(:view_profile).and_return (fake_results)
-    post :view_profile
-    expect(assigns(:profiles)).to eq(fake_results)
+  # Sign Up Page
+  describe 'Sign Up Page' do
+
   end
-  describe 'sign up page' do
-    it 'should call the model that shows profiles info' do
-      allow(MainController).to receive(:sign_up)
-      post :sign_up
-      expect(response).to render_template('sign_up')
-    end
-    it 'should make the profiles info available to that template' do
-      fake_results = [double('Profile'), double('Profile')]
-      allow(MainController).to receive(:sign_up).and_return (fake_results)
-      post :sign_up
-      expect(assigns(:profiles)).to eq(fake_results)
-    end
+  # Edit Profiles Page
+  describe 'Edit Profiles Page' do
+
   end
-  describe 'edit profiles page' do
-    it 'should call the model that shows profiles info' do
-      allow(MainController).to receive(:edit_profile)
-      post :edit_profile
-      expect(response).to render_template('edit_profile')
-    end
-    it 'should make the profiles info available to that template' do
-      fake_results = [double('Profile'), double('Profile')]
-      allow(MainController).to receive(:edit_profile).and_return (fake_results)
-      post :edit_profile
-      expect(assigns(:profiles)).to eq(fake_results)
-    end
-  end
-  describe 'login page' do
-    it 'should call the model that shows profiles info' do
-      allow(MainController).to receive(:login)
-      post :login
-      expect(response).to render_template('login')
-    end
-    it 'should make the profiles info available to that template' do
-      fake_profile = double('Profile', id: 1, student_id: 1, gre: 1, toefl: 1,
-                            capa: 1, interested_major: 'Computer Science and Engineering', term: '1',
-                            year: '2023', college_name: 'University of Iowa')
-      allow(MainController).to receive(:login).and_return(fake_profile)
-      post :login
-      expect(assigns(:profiles)).to eq(fake_profile)
-    end
+  # Login Page
+  describe 'Login Page' do
+
   end
   # Search Universities Page
   describe 'Search Universities' do
@@ -116,7 +78,7 @@ describe MainController do
       expect(flash[:notice]).to eq('Please fill out all fields')
     end
   end
-  # view_university
+  # View University
   describe 'View University' do
     it 'should redirect to the home page if student not logged in' do
       allow(MainController).to receive(:view_university)
