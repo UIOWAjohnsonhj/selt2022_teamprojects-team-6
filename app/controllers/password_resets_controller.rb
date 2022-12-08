3b47439274c08744a51f23631f185113d4a83a2f
aezouhri <adnane-ezouhri@uiowa.edu>
2022-12-07 19:41:16 -0600

    Controller runs properly without calling any error and calling the nailer class.

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,65 +1,8 @@
-class PasswordResetsController < ApplicationController
-  require 'net/smtp'
-  FROM_EMAIL = "ezouhriadnane@outlook.com"
-  PASSWORD = "Takamatika971!"
-  TO_EMAIL = "ezouhriadnane@gmail.com"
-
-  def create
-    puts "email sent1 "
-    ExampleMailer.simple_message("ezouhriadnane@gmail.com")
-    puts "email sent2 "
-
-#     smtp = Net::SMTP.new 'smtp.gmail.com', 587
-#     message = <<END_OF_MESSAGE
-#     From: John Doe <mail@example.com>
-#     To: Adnane Ezouhri <ezouhriadnane@gmail.com>
-#     Subject: Sending email with Ruby Hello. This is an email sent with Ruby.
-# END_OF_MESSAGE
-#
-#     smtp.start('fitwalkcode@outlook.com', FROM_EMAIL, PASSWORD, :plain)
-#     smtp.send_message(message, FROM_EMAIL, TO_EMAIL)
-#     smtp.finish()
-#     token = generate_base64_token
-#     @user.update!(recovery_password: token)
-#     if @user.recovery_password_digest != nil
-#       PasswordMailer.password_reset(@user).deliver_now
-#       render json: {alert: "Your custom alert here."}
-#     else
-#       render json: {error: "Your custom error message here."}
-#     end
-#     redirect_to '/login', notice: 'If an account with that email was found, we have sent a link to reset password'
+class PasswordResetsController
 
 
+  def send_password_reset
+    PasswordMailer.password_reset("ezouhriadnane@gmail.com").deliver_now
   end
 
-  def edit
-    #change the user to student and faculty_member
-    # for now am testing with the student, probably gonna expand to faculty_member
-    @user = Student.find_signed!(params[:token], purpose: 'password_reset')
-  rescue ActiveSupport::MessageVerifier::InvalidSignature
-    redirect_to '/login', alert: 'Your token has expired. Please try again'
-  end
-
-
-  def update
-    #change the user to student and faculty_member
-    # for now am testing with the student, probably gonna expand to faculty_member
-    @user = Student.find_signed!(params[:token], purpose: 'password_reset')
-    if @user.update(password_params)
-      redirect_to '/login', notice: 'Your password was reset succesfully. Please sign in.'
-    else
-      render 'edit'
-    end
-  rescue ActiveSupport::MessageVerifier::InvalidSignature
-    redirect_to '/login', alert: 'Your token has expired. Please try again'
-  end
-
-end
-
-
-class ExampleMailer < ActionMailer::Base
-  default from: 'ezouhriadnane@outlook.com'
-  def simple_message(recipient)
-    mail(to: recipient,subject: 'Sending email with Ruby',body: 'Hello. This email is sent with Ruby.')
-    end
 end
--- app/mailers/password_mailer.rb
+++ app/mailers/password_mailer.rb
@@ -1,25 +1,9 @@
-class PasswordMailer < ApplicationMailer
-  default from: 'mail@example.com'
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.password_mailer.reset.subject
-  #
-  # def reset()
-  #   #@user = 'fitwalkcode@outlook.com' #params[:user]
-  #   #@token = @user.signed_id(purpose: 'password',expires_in:15.minutes) #this token has time to live of 15 minutes
-  #   #@url = 'http://example.com/login'
-  #   recipient = 'fitwalkcode@outlook.com'
-  #   puts 'here are i am line 13'
-  #   mail(to: recipient,subject: 'Sending email with Ruby',body: 'Hello. This email is sent with Ruby.')
-  #   #mail to: @user.email
-  #
-  # end
-  def simple_message(recipient)
-    mail(
-      to: recipient,
-      subject: 'Sending email with Ruby',
-      body: 'Hello. This email is sent with Ruby.'
-    )
+class PasswordMailer < ActionMailer::Base
+  default from: 'simplapply5@gmail.com'
+
+  def password_reset(user_email)
+    @user= user_email
+
+    mail(to: @user.email, subject:"Subject")
   end
 end
