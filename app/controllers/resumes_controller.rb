21e107cfa7be82851fe545e8d32ec08ce4c7ee7a
Ben DeSollar <ben.desollar01@gmail.com>
2022-12-07 20:09:26 -0600

    Fixed some bugs and updated the resume model and controller

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -13,6 +13,7 @@ class ResumesController < ApplicationController
     @student = Student.find_by(id: current_student.id)
     Resume.destroy_old_resume(@student.id)
     if @resume.save
+      @resume.update(student_id: current_student.id)
       redirect_to view_profile_path(@student, student_id: @student.id), notice: "The resume #{@resume.name} has been uploaded."
     else
       render "new"
@@ -21,15 +22,15 @@ class ResumesController < ApplicationController
   end
 
   def destroy
-    @resume = Resume.find(session[:student_id])
-    @student = Student.find_by(id: session[:student_id])
+    @resume = Resume.find(current_student.id)
+    @student = Student.find_by(id: current_student.id)
     @resume.destroy
     redirect_to resumes_path(@student, student_id: @student.id), notice:  "The resume #{@resume.name} has been deleted."
   end
 
   private
   def resume_params
-    params.require(:resume).permit(:name, :student_id, :attachment)
+    params.require(:resume).permit(:name, :attachment)
   end
 
 end
