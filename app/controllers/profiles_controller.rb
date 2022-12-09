f564717377215d04febd8352e480c51fdff9eda9
Ben DeSollar <ben.desollar01@gmail.com>
2022-12-08 23:08:20 -0600

    Added in professor being able to see all the application they have commented and made a decision for.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -8,14 +8,8 @@ class ProfilesController < ApplicationController
     @experiences = Experiences.where(student_id: params[:student_id])
     @resume = Resume.where(student_id: params[:student_id]).take
     @evaluations = Evaluation.where(student_id: params[:student_id])
-    if @evaluation != nil
-      @evaluation = Evaluation.where(student_id: params[:student_id]).take
-
-
-    end
     if @resume.nil?
       @resume = "No resume uploaded"
     end
-
   end
 end
