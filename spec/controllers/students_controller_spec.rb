d854a239cc229994676311e45c112a533efb831c
erbrewer <erbrewer@uiowa.edu>
2022-12-10 13:54:36 -0600

    TST: change_page working with devise.

--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -23,17 +23,27 @@ describe MainController do
   # View Profile
   describe 'View Profile Page' do
     it 'should redirect to the home page if not logged in' do
-
+      allow(MainController).to receive(:view_profile)
+      get :view_profile
+      expect(response).to redirect_to('/')
     end
     it 'should show the profile page if logged in' do
-
+      allow_any_instance_of(Devise::Controllers::Helpers).to receive(:student_signed_in?).and_return(true)
+      allow(MainController).to receive(:view_profile)
+      get :view_profile
+      expect(response).to redirect_to('view_profile')
+    end
+    it 'should set the @current_profile variable' do
+      allow_any_instance_of(Devise::Controllers::Helpers).to receive(:student_signed_in?).and_return(true)
+      allow(MainController).to receive(:view_profile)
+      get :view_profile, params: { id: 1 }
     end
   end
   # Add Experience
   describe 'Add Experience Page' do
     it 'should redirect to the edit profile page on post' do
       allow(MainController).to receive(:add_experience)
-      post :add_experience, params: { company: "Test", title: "Test", description: "Test", from: "01-01-2000", to: "01-01-20001" }
+      post :add_experience, params: { company: "Test", title: "Test", description: "Test", from: "01-01-2000", to: "01-01-20001" }, session: { experience_count: 0 }
       expect(response).to redirect_to('/edit_profile')
     end
     it 'should flash an error if all fields aren\'t filled out' do
@@ -132,6 +142,34 @@ describe MainController do
       allow(MainController).to receive(:search_instructor)
       get :search_instructor
       expect(response).to render_template('search_instructor')
+    end
+  end
+  # change_page action
+  describe 'change_page' do
+    it 'should redirect to the home page if student not logged in' do
+      allow(MainController).to receive(:change_page)
+      get :change_page
+      expect(response).to redirect_to('/')
+    end
+    it 'should redirect to search_universities if logged in' do
+      allow_any_instance_of(Devise::Controllers::Helpers).to receive(:student_signed_in?).and_return(true)
+      allow(MainController).to receive(:change_page)
+      get :change_page, params: { prev: 1 }, session: { page_counter: 2 }
+      expect(response).to redirect_to('/search_universities')
+    end
+    it 'should increment the page counter' do
+      allow_any_instance_of(Devise::Controllers::Helpers).to receive(:student_signed_in?).and_return(true)
+      allow(MainController).to receive(:change_page)
+      get :change_page, params: { prev: 1 }, session: { page_counter: 2 }
+      expect(response).to redirect_to('/search_universities')
+      expect(session[:page_counter]).to eq(1)
+    end
+    it 'should decrement the page counter' do
+      allow_any_instance_of(Devise::Controllers::Helpers).to receive(:student_signed_in?).and_return(true)
+      allow(MainController).to receive(:change_page)
+      get :change_page, params: { }, session: { page_counter: 1, university_count: 20}
+      expect(response).to redirect_to('/search_universities')
+      expect(session[:page_counter]).to eq(2)
     end
   end
 end
