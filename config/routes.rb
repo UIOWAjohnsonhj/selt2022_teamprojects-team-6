bb04fd7a369bd2a1f17fa3493ff94f6c9aa95c3a
aezouhri <adnane-ezouhri@uiowa.edu>
2022-12-10 21:12:30 -0600

    Cucumber logic is there, however due to devise I think I can't route to pages. Will keep working on it.

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   get 'resumes/destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "main#index"
+  root to: "main#index", as: "main_index"
 
   get 'login', to: 'main#login', as: 'login'
   post 'main/login'
